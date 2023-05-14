4a18f07167a05c814ac21d278a2f44a8e43aa6d4
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-14 13:28:56 -0700

    kept adding ppackages into wrong terminal so after downloading into the correct folders I then called in the proper objkects within the server js file within server folder

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//bringing in the proper objects to start off the transition from REST to Apollo
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//creating an instance of the apollo server
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +25,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+
 
 app.use(routes);
 
