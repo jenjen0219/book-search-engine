f5bdb11042e5b8093d0c75d923d2aef78e96226d
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-14 23:16:32 -0700

    removed several files that are now completely replaced with updated code tailored to graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
-        <Routes>
+        <Switch>
           <Route 
             path='/' 
             element={<SearchBooks />} 
@@ -22,9 +50,10 @@ function App() {
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
-        </Routes>
+        </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
